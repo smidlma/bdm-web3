89a139421d911d80c38e83c410b2f8709bddf7fc
Martin Šmídl <smidl.martin1@gmail.com>
2024-05-06 19:10:37 +0200

    chore: Update page title and base URL

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/bdm-web3/',
+  server: {
+    open: true,
+  },
   plugins: [react()],
 })
